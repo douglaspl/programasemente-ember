aaf7b4e0324954b5af9a0058a061fe4556dd8cc5
douglaspl <douglas.linhares@coreskills.com.br>
2019-04-11 14:05:58 -0300

    Environments apontados para a api na nuvem

--- config/environment.js
+++ config/environment.js
@@ -30,7 +30,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'development') {
-    ENV.APP.host = 'http://localhost:64568';
+    ENV.APP.host = 'https://sementeapimedio.minimo.com.br';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
