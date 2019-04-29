65e0a5f62bcfe8136615c1069ba351d9b47ab99c
Gabriel V. S. Sousa <Gabrielvss7@hotmail.com>
2019-04-29 11:18:18 -0300

    Linha de endereço lucalhost adicionada no environment (comentada)

--- config/environment.js
+++ config/environment.js
@@ -31,6 +31,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.APP.host = 'https://sementeapimedio.minimo.com.br';
+    // ENV.APP.host = 'http://localhost:64568';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
