1fa14a6a54ac9721a948d6fdd9042e12961cd62a
Gabriel V. S. Sousa <Gabrielvss7@hotmail.com>
2019-12-20 10:39:08 -0300

    fix tela branca

--- app/controllers/firstaccess.js
+++ app/controllers/firstaccess.js
@@ -50,11 +50,21 @@ export default Controller.extend({
               let institutions = included.filter(function (i) {
                 return i.id === log.instituicao_id;
               });
+              if (!institutions[0]){
+                localStorage.clear();
+                window.location.reload(true);
+                return
+              }
               let institutionChangePasswordParam = institutions[0].attributes.trocasenhaobrigatoria;
               // ---------------------------------------- account param
               let accounts = data.filter(function (i) {
                 return i.id === log.id;
               });
+              if (!accounts[0]){
+                localStorage.clear();
+                window.location.reload(true);
+                return
+              }
               let accountChangePasswordParam = accounts[0].attributes.trocousenha;
               // ----------------------------------------- verification
               if (institutionChangePasswordParam === true)
