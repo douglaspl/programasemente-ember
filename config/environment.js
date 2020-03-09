df9d92e0626002129b9e64ef670e452957a3762b
Gabriel V. S. Sousa <Gabrielvss7@hotmail.com>
2020-03-09 13:13:30 -0300

    fix bugs fast

--- config/environment.js
+++ config/environment.js
@@ -21,9 +21,9 @@ module.exports = function(environment) {
 
     APP: {
 
-      //host: 'http://localhost:64568',
+      host: 'http://localhost:64568',
       // host:  'https://sementeapi.minimo.com.br',
-      host:  'http://sementeapidev.minimo.com.br',
+      // host:  'http://sementeapidev.minimo.com.br',
       namespace: 'api/v0',
       // Here you can pass flags/options to your application instance
       // when it is created
@@ -32,7 +32,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.host = 'https://sementeapi.minimo.com.br';
-    ENV.APP.host = 'http://sementeapidev.minimo.com.br';
+    // ENV.APP.host = 'http://sementeapidev.minimo.com.br';
     // ENV.APP.host = 'http://localhost:64568';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
