f296b26ed06ddeacc3826b1fba7e507a46035ed9
Gabriel V. S. Sousa <Gabrielvss7@hotmail.com>
2020-10-08 15:59:59 -0300

    .

--- app/models/aplicacao-plataforma-aula.js
+++ app/models/aplicacao-plataforma-aula.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    name: DS.attr(),
     dataAplicacao: DS.attr(),
     pessoa: DS.belongsTo('pessoa', { async: true }),
     turma: DS.belongsTo('plataforma-turma', { async: true }),
