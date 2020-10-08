12266f86d472a0081af1d83c1da7dcce505da96d
Brunoporto2702 <brunop2702@gmail.com>
2020-10-08 17:37:53 -0300

    finaliza pagina aluno fund2

--- app/models/aplicacao-plataforma-aula.js
+++ app/models/aplicacao-plataforma-aula.js
@@ -2,7 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     dataAplicacao: DS.attr(),
+    dataAplicacaoFormat: Ember.computed('dataAplicacao', function() {
+        return moment(this.get('dataAplicacao'), 'X').format('DD/MM/YYYY');
+      }),
     pessoa: DS.belongsTo('pessoa', { async: true }),
     turma: DS.belongsTo('plataforma-turma', { async: true }),
-    aula: DS.belongsTo('aula', { async: true })
+    aula: DS.belongsTo('aula', { async: true }),
 });
