197a0be3d032694e797e7e7786da4f8195fa6a34
Gabriel V. S. Sousa <Gabrielvss7@hotmail.com>
2020-11-11 10:54:59 -0300

    .

--- app/controllers/autoregister/index.js
+++ app/controllers/autoregister/index.js
@@ -4,22 +4,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   store: Ember.inject.service(),
   session: Ember.inject.service('session'),
-  transitionToForm: function(e) { this.transitionToRoute('autoregister.form', e.get('id')); },
+  // transitionToForm: function(e) { this.transitionToRoute('autoregister.form', e.get('id')); },
   actions: {
-    makeAjax(){
-    },
     checkForm(){
       $('form').removeData('validator');
       $('form').removeData('unobtrusiveValidation');
       $.validator.unobtrusive.parse('form');
       let that = this;
-
+      debugger;
       let schoolCode = document.getElementById('codigo-escola').value;
       let codigo = this.get('store').queryRecord('codigo-cadastro', {
         include: 'instituicao.plataforma-anos, instituicao.plataforma-turmas',
         codigo: schoolCode
       }).then(function(e){
-        that.transitionToForm(e);
+        // that.transitionToForm(e);
+        that.transitionToRoute('autoregister.form', e.get('id'));
       }).catch(function(error) {
         if (error.errors) {
           console.log(error.massage);
