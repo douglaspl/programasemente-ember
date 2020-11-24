67ca458ea3a0b4272d2ed6bc933cc826a5816b91
MorenoRebeca <rebsmoreno13@gmail.com>
2020-11-24 19:04:27 -0300

    Add elementos conteudo especifico

--- app/models/aula.js
+++ app/models/aula.js
@@ -1,11 +1,26 @@
 import DS from 'ember-data';
 
+moment.updateLocale('en', {
+    monthsShort : [
+        "jan", "fev", "mar", "abr", "maio", "jun",
+        "jul", "ago", "set", "out", "nov", "dez"
+    ]
+});
+
 export default DS.Model.extend({
     titulo: DS.attr(),
     descricao: DS.attr(),
     idx: DS.attr(),
     dataInicioPrevista: DS.attr(),
+    dataInicioPrevistaFormat: Ember.computed('dataInicioPrevista', function() {
+        let dataformatada = moment(this.get('dataInicioPrevista'), 'DD/MM/YYYY hh:mm:ss').format('DD MMM');
+        return dataformatada;
+    }),
     dataFimPrevista: DS.attr(),
+    dataFimPrevistaFormat: Ember.computed('dataFimPrevista', function() {
+        let dataformatada = moment(this.get('dataFimPrevista'), 'DD/MM/YYYY hh:mm:ss').format('DD MMM');
+        return dataformatada;
+    }),
     plataformaConteudos: DS.hasMany('plataforma-conteudo', { async: true }),
     plataformaAno: DS.belongsTo('plataforma-ano', { async: true }),
     unidade: DS.belongsTo('unidade', { async: true }),
