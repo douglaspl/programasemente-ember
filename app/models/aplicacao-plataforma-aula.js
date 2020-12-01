2e782518ebd2c6e91f7e08644e9222e6ae1d6d75
Rebeca Moreno <rebsmoreno13@gmail.com>
2020-12-01 18:46:20 -0300

    aula específica - aplicacao

--- app/models/aplicacao-plataforma-aula.js
+++ app/models/aplicacao-plataforma-aula.js
@@ -1,11 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    dataAplicacao: DS.attr(),
-    dataAplicacaoFormat: Ember.computed('dataAplicacao', function() {
-        return moment(this.get('dataAplicacao'), 'X').format('DD/MM/YYYY');
-      }),
-    pessoa: DS.belongsTo('pessoa', { async: true }),
-    turma: DS.belongsTo('plataforma-turma', { async: true }),
-    aula: DS.belongsTo('aula', { async: true }),
+  dataAplicacao: DS.attr(),
+  aplicado: DS.attr('boolean'),
+  dataAplicacaoFormat: Ember.computed('dataAplicacao', function() {
+    return moment(this.get('dataAplicacao'), 'X').format('DD/MM/YYYY');
+  }),
+  pessoa: DS.belongsTo('pessoa', { async: true }),
+  turma: DS.belongsTo('plataforma-turma', { async: true }),
+  aula: DS.belongsTo('aula', { async: true }),
 });
