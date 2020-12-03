0adc68f284f7c398a2f0f3dda61dd49797e4ea9f
Rebeca Moreno <rebsmoreno13@gmail.com>
2020-12-03 11:19:11 -0300

    Home Aula Index e específica

--- app/models/aplicacao-plataforma-aula.js
+++ app/models/aplicacao-plataforma-aula.js
@@ -1,10 +1,18 @@
 import DS from 'ember-data';
 
+moment.updateLocale('en', {
+  monthsShort : [
+    "jan", "fev", "mar", "abr", "maio", "jun",
+    "jul", "ago", "set", "out", "nov", "dez"
+  ]
+});
+
 export default DS.Model.extend({
   dataAplicacao: DS.attr(),
   aplicado: DS.attr('boolean'),
   dataAplicacaoFormat: Ember.computed('dataAplicacao', function() {
-    return moment(this.get('dataAplicacao'), 'X').format('DD/MM/YYYY');
+    let dataformatada = moment(this.get('dataAplicacao'), 'DD/MM/YYYY hh:mm:ss').format('DD MMM');
+    return dataformatada;
   }),
   pessoa: DS.belongsTo('pessoa', { async: true }),
   turma: DS.belongsTo('plataforma-turma', { async: true }),
